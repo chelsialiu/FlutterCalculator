94cb2c0149dcc585e30f1c60ffea6061a8beb613
chelsialiu <43692077+chelsialiu@users.noreply.github.com>
2020-01-11 18:53:58 -0700

    Created new Material App with app title, theme, and homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+//Created by chelsliu on 1/11/20
+
 import 'package:flutter/material.dart';
+import 'home_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,21 +9,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    return new MaterialApp(
+        title: 'Calculator App',
+        theme: new ThemeData.dark(),
+        home: new HomePage(),
     );
   }
 }
